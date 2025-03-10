b19bd26098fceaf8c111b3b6dabcc983958f5d4f
shah-khush <152987034+shah-khush@users.noreply.github.com>
2025-03-10 10:27:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ with open("best_model.pkl", "rb") as f:
 st.title("Diabetes Prediction App")
 st.write("Enter the patient's details below to get a prediction:")
 
+pregnancy = st.number_input("Pregnancy 1=yes 0=no", value=1, min_value=0, max_value=1)
 glucose = st.number_input("Glucose", value=120, min_value=0)
-blood_pressure = st.number_input("Blood Pressure", value=70, min_value=0)
-bmi = st.number_input("BMI", value=30.0, min_value=0.0)
 insulin = st.number_input("Insulin", value=80, min_value=0)
+bmi = st.number_input("BMI", value=30.0, min_value=0.0)
 age = st.number_input("Age", value=35, min_value=0)
 
 if st.button("Predict"):
-    input_data = np.array([[glucose, blood_pressure, insulin, bmi, age]])
+    input_data = np.array([[pregnancy, glucose, insulin, bmi, age]])
     prediction = model.predict(input_data)
     
     if prediction[0] == 1:
